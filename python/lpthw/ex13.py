0bb9aaf54511a5ab12285511b438a72bba58acd0
gdhu <guodong_hu@126.com>
2020-05-01 07:05:50 +0800

    py

---
+++ python/lpthw/ex13.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called:", script)
+
+print("Your first variable is:", first)
+
+print("Your second variable is:", second)
+
+print("Your third variable is:", third)
+
+age = int(input('please input you age '))
+
+print("Your age is:", age)
